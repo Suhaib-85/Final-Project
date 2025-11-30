89be85b839495dcbf43ce2347b54691f8cc22996
Suhaib <sohaibsaleem858@gmail.com>
2025-11-30 21:02:09 +0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,13 +1,10 @@
-// config/index.js
-import 'dotenv/config'; // Use /config to load variables globally
+import 'dotenv/config';
 
 const config = {
-    PORT: process.env.PORT || 5000,
+    PORT: Number(process.env.PORT) || 5000,
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
-    REDIS_HOST: process.env.REDIS_HOST,
-    REDIS_PORT: process.env.REDIS_PORT,
-    REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+    REDIS_URL: process.env.REDIS_URL, // <-- add this
     FRONTEND_URL: process.env.FRONTEND_URL,
     FASTAPI_SYNC_URL: process.env.FASTAPI_SYNC_URL
 };
